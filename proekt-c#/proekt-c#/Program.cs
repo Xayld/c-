9c8a0c97c76acea7992499bbb6ebf70c30ad193b
ᴺᵉˣᵘˢÆ_Irus <131009874+Irus07@users.noreply.github.com>
2025-04-02 21:55:12 +0300

    Update Program.cs

--- proekt-c#/proekt-c#/Program.cs
+++ proekt-c#/proekt-c#/Program.cs
@@ -1,53 +1,39 @@
-﻿using System.ComponentModel.Design;
+using System.ComponentModel.Design;
 using System.Runtime.Intrinsics.X86;
 
 namespace proekt_c_
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите Первое число: ");
+			int firstnum = int.Parse(Console.ReadLine()); // парсим string в int
+			Console.Write("Введите Второе число: ");
+			int secondnum = int.Parse(Console.ReadLine());// парсим string в int
+			Console.Write("Введите действия(Деление, умножение, сложение, вычитание): ");
+			string operation = Console.ReadLine();
 
+			switch (operation.ToLower() /* не учитываем регистр введённой операции */)
+			{
+				case "деление":
+					Console.WriteLine("Ответ: " + (firstnum / secondnum));
+					break;
 
-            Console.Write("Введите Первое число: ");
-            string numberOne = Console.ReadLine();
-            Console.Write("Введите Второе число: ");
-            string numberTwo = Console.ReadLine();
-            Console.Write("Введите действия(Деление, умножение, сложение, вычитание): ");
-            string type = Console.ReadLine();
+				case "умножение":
+					Console.WriteLine("Ответ: " + (firstnum * secondnum));
+					break;
 
-            int oneToInt = Convert.ToInt32(numberOne);
-            int twoToInt = Convert.ToInt32(numberTwo);
+				case "сложение":
+					Console.WriteLine("Ответ: " + (firstnum + secondnum));
+					break;
 
-            if (type == "Деление")
-            {
-                Console.Write("Ответ: ");
-                Console.Write(oneToInt / twoToInt);
+				case "вычитание":
+					Console.WriteLine("Ответ: " + (firstnum - secondnum));
+					break;
+			}
 
-
-            } else if (type == "Умножение") {
-                Console.Write("Ответ: ");
-                Console.Write(oneToInt * twoToInt);
-
-            } else if (type == "Сложение")
-
-            {
-                Console.Write("Ответ: ");
-                Console.Write(oneToInt + twoToInt);
-            } else if (type == "Вычитание")
-            {
-                Console.Write("Ответ: ");
-                Console.Write(oneToInt - twoToInt);
-            };
-
-            Console.ReadLine();
-
-
-
-
-
-
-
-        }
-    }
+			Console.ReadLine();
+		}
+	}
 }
